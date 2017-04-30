e9cd03408d70108ad3fe9656ab6c2819d455dce7
Scott Chen <one5809@gmail.com>
2017-04-30 10:16:11 +0800

    camera look poland

--- www/js/norad_tle_satellites_no_debris.js
+++ www/js/norad_tle_satellites_no_debris.js
@@ -1,37 +1,5 @@
 KMG.ORBITS = [
 	{
-		name : "TEST",
-		entries : [
-			{
-				name : "Test-1",
-				satelliteNumber : 0,
-				classification : "U",
-				internationalDesignator : "97020A",
-				launchYear : 97,
-				launchNumberForYear : 020,
-				launchPieceNumber : "A",
-				epochYear : 14.0,
-				epochDay : 101.40472434,
-				derivativeOfMeanMotion : 2.79e-06,
-				meanMotion : 14.34209898,
-				dragTerm : "0.92590-4",
-				ephemerisType : 0,
-				semiMajorAxis : 7155.80566796,
-				longitudeOfPerihelion : 0,
-				eccentricity : 0.0002214,
-				inclination : 46.3942,
-				ascendingNode : 84.7455, 
-				argOfPeriapsis : 101.4814,
-				meanAnomalyAtEpoch : 258.6631,
-				period : 0.0697247931025,
-				isDebris : "no",
-				epoch : 2456758.90472,
-				epochStart : 5215.40472434,
-				epochNow : 5216.00084491
-			}
-		]
-	},
-	{
 		name : "POLAND",
 		entries : [
 			{
